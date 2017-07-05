822a5ee994df5e3b5cbada2d5518e0856361b6e1
Gavin Ridley <gridley@vols.utk.edu>
2017-07-05 12:22:55 -0500

    finished GC generation tutorial

--- tutorial/step01_groupConstants/README.md
+++ tutorial/step01_groupConstants/README.md
@@ -10,3 +10,27 @@ own infinite universe. Then, this universe should have its group constants gener
 It's important that universe 0, the main universe, is not included. Serpent takes tallies for group constants in the first universe
 it identifies, so including 0 means that no further universes will be included in GC generation. (please double check this on the serpent
 forum).
+
+### Serpent Input
+
+Here, serpent input can be found to simulate ORNL's molten salt reactor experiment, documented largely on websites like
+(this)[http://www.energyfromthorium.com/pdf/] one.
+
+For group constant generation, Cole Gentry found that a 4 group structure with three thermal groups and one fast group works
+well in the graphite moderated FHR. There is also an available recommended 13 group structure, which could be found by changing
+some comment characters around in the include file defining group structures. The PDF of these findings can be found (here)[http://trace.tennessee.edu/utk_graddiss/3695/].
+
+The "fuel" file contains "cards" (a relic term referring to the fortran-dominated reactor physics ages of yore)
+that will generate group constants at a few temperatures, and likewise for the "moder" file.
+Like a cooking show, the important results have already been prepared for you in the "fuel.coe" and "moder.coe" files, which
+get parsed by PyNE in the extractSerpent2GCs script.
+
+### Command to run:
+
+The command to run in order to generate the moltres-compatible group constants is:
+
+```./extractSerpent2GCs.py MSREProperties msre_gentry_4g tempMapping.txt universeMapping.txt fuel moder```
+
+The syntax requires the arbitrary directory name you'd like to create, then the arbitrary file base name that moltres will
+look at, then a file that maps branch names to temperatures, then a file that maps universe numbers from serpent to material names,
+and lastly the list of all materials you'd like to pass group constants into moltres for.
--- tutorial/step01_groupConstants/extractSerpent2GCs.py
+++ tutorial/step01_groupConstants/extractSerpent2GCs.py
@@ -16,106 +16,88 @@ except ImportError as err:
     print( err )
     quit()
 
-def makePropertiesFromSerpentOutput(outfile, outdir, serp1=False, useB1=False, uni2matname=None, fromMain = False, usen2n=True):
-    """ Takes serpent 2 output from a _res.m file, and translates 
-    into moltres-compatible neutronics parameters.
-
-    Args:
-        outfile -- serpent output, *_res.m. Should have group constants
-        outdir  -- the moltres property dir to be created
-
-    Kwargs:
-        serp1   -- bool. if true, assumes that group scattering matrix is transposed.
-                    defaults to false.
-
-        useB1   -- bool. if true, uses B1-generated group constants. These are a
-                    spectrum-adjusted technique for making infinite lattice group
-                    constants work for whole-core.
-                    defaults to false.
-
-        uni2matname -- dict. set this if you're running this function from python, not terminal.
-                        makes human-readable input for moltres. Otherwise it would be hard to
-                        tell which material is which. Maps a universe name in string form to
-                        the new name. e.g. {'1':'fuel','2':'moder'}
-
-        fromMain -- don't use this. for this script's use.
-
-        usen2n -- bool. defaults to true. Whether to include scattering production reactions
-                        like (n,2n) in the group scattering matrix.
-
-
-
-    Returns:
-        tuple[2]-- number of groups found (int)
-    """
+def makePropertiesDir(inmats, outdir, filebase, mapFile, unimapFile, serp1=False, fromMain=False):
+    """ Takes in a mapping from branch names to material temperatures,
+    then makes a properties directory.
+    Serp1 means that the group transfer matrix is transposed."""
 
     if serp1:
-        raise NotImplementedError("please have someone eg gavin add this")
+        raise NotImplementedError("C'mon, just get serpent 2!")
 
-    # try all the i/o first
-    try:
-        infile = open(outfile, 'r')
+    if not os.path.isdir(outdir):
         os.mkdir(outdir)
-    except IOError as err:
-        print("Looks like some args don't exist, or you don't have the right")
-        print("permissions.")
-        print( err )
-        quit()
+    print("Making properties for materials:")
+    print(inmats)
+    coeList = dict([(mat,sss.parse_coe(mat+'.coe')) for mat in inmats])
 
-    # a dictionary of serpent output:
-    sssOut = sss.parse_res(infile)
+    # the constants moltres looks for:
+    goodStuff = ['BETA_EFF','CHI','DECAY_CONSTANT','DIFF_COEF','FISSE','GTRANSFXS','NSF','RECIPVEL','REMXS']
+    goodMap   = dict([(thing, 'INF_'+thing) for thing in goodStuff])
 
-    # whether to use B1
-    # this should only be used if a unit cell was done in serpent
-    prefix = 'INF_' if not useB1 else 'B1_'
+    # the name for the group transfer XS matrix is different in serpent
+    # and moltres, so this gets fixed:
+    goodMap['GTRANSFXS'] = 'INF_SP0' # scattering + production in 0 legendre moment
+    goodMap['BETA_EFF'] = 'BETA_EFF'
+    goodMap['CHI'] = 'INF_CHIP' # include production. chip, chit, or chid???
+    goodMap['DECAY_CONSTANT']='LAMBDA'
+    goodMap['RECIPVEL'] = 'INF_INVV'
+    goodMap['DIFF_COEF'] = 'INF_DIFFCOEF'
+    goodMap['FISSE'] = 'INF_KAPPA'
 
-    # count how many universes had GCs generated:
-    nUni = len(sssOut['GC_UNIVERSE_NAME'])
+    # map material names to universe names from serpent
+    with open(unimapFile) as fh:
+        uniMap = []
+        for line in fh:
+            uniMap.append(tuple(line.split()))
+    # this now maps material names to serpent universes
+    uniMap = dict(uniMap)
 
-    # now, map universe names to moltres material names, if desired.
-    # this just makes the output prettier.
-    if uni2matname == None and fromMain:
-        uni2matname = dict.fromkeys(sssOut['GC_UNIVERSE_NAME'], None)
-        for uni in uni2matname.keys():
-            matName= input("Please give a material name for universe {}, then hit enter:\n".format(uni))
-            uni2matname[uni] = matName
-    elif unit2matname == None:
-        print("See pydoc <thisfile>.makePropertiesFromSerpentOutput")
-        raise Exception("If you're running this as a function, set a uni2matname map through the kwargs.")
+    branch2TempMapping = open(mapFile)
+    for line in branch2TempMapping:
 
-    # now, look through it for the good stuff (GCs):
-    scatMat = 'SP0' if usen2n else 'P0'
-    goodStuff = ['REMXS', 'FISSXS', 'NSF', 'FISSE', 'DIFFCOEF', 'RECIPVEL', scatMat]
-    # by the way, NSF -> nu * Sigma_{fission}
-    
+        item, temp = tuple(line.split())
+        for mat in inmats:
+            if mat in item:
+                currentMat = mat
+                break
+        else:
+            print('Considered materials: {}'.format(inmats))
+            raise Exception('Couldnt find a material corresponding to branch {}'.format(item))
 
+        for coefficient in goodStuff:
+            with open(outdir+'/'+filebase+currentMat+'_'+coefficient, 'a') as fh:
+                strData = coeList[currentMat][1][uniMap[currentMat]][item]["rod0"][goodMap[coefficient]]
+                if coefficient == 'DECAY_CONSTANT' or coefficient == 'BETA_EFF':
+                    # some additional formatting is needed here
+                    strData = strData[1:7]
+                strData = ' '.join([str(dat) for dat in strData]) if isinstance(strData,list) else strData
+                fh.write(str(temp)+' '+strData)
+                fh.write('\n')
 
+    return None
 
-
-
-    infile.close()
-
-    # might as well return something, bc why not (eg debug)
-    return sssOut
 
 if __name__ == '__main__':
 
     # make it act like a nice little terminal program
     parser = argparse.ArgumentParser(description='Extracts Serpent 2 group constants, and puts them in a directory suitable for moltres.')
-    parser.add_argument('serpentOutputFile', metavar='i', type=str, nargs='+', help='string describing a path to the serp output')
-    parser.add_argument('outDir', metavar='o', type=str, nargs='+', help='name of directory to write properties to.')
+    parser.add_argument('outDir', metavar='o', type=str, nargs=1, help='name of directory to write properties to.')
+    parser.add_argument('fileBase', metavar='f', type=str, nargs=1, help='File base name to give moltres')
+    parser.add_argument('mapFile' , metavar='b', type=str, nargs=1, help='File that maps branches to temperatures')
+    parser.add_argument('universeMap', metavar='u', type=str, nargs=1, help='File that maps material names to serpent universe')
+    parser.add_argument('materials', metavar='m', type=str, nargs='+', help='list of moltres material names')
     parser.add_argument('--serp1', dest='serp1', action='store_true', help='use this flag for serpent 1 group transfer matrices')
     parser.set_defaults(serp1=False)
 
     args = parser.parse_args()
 
-    if len(args.serpentOutputFile)>1 or len(args.outDir)>1:
-        raise NotImplementedError("Automatically making several property dirs in the future may be supported, but not now.")
-
-    # these are unpacked,so it fails if they werent passed to the script
-    infile = args.serpentOutputFile[0]
+    # these are unpacked, so it fails if they werent passed to the script
+    inmats = args.materials
     outdir = args.outDir[0]
+    fileBase = args.fileBase[0]
+    mapfile = args.mapFile[0]
+    unimapfile = args.universeMap[0]
 
-    makePropertiesFromSerpentOutput(infile, outdir, serp1=args.serp1, fromMain = True)
+    makePropertiesDir(inmats, outdir, fileBase, mapfile,unimapfile, serp1=args.serp1, fromMain = True)
 
     print("Successfully made property files in directory {}.".format(outdir))
